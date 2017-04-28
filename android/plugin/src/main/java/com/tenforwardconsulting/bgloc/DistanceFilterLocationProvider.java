0fa1f49ec48ece129026ebfc338b2c6cebe8188b
danielpgauer <danielpgauer@gmail.com>
2017-04-28 09:28:03 -0300

    test

--- android/plugin/src/main/java/com/tenforwardconsulting/bgloc/DistanceFilterLocationProvider.java
+++ android/plugin/src/main/java/com/tenforwardconsulting/bgloc/DistanceFilterLocationProvider.java
@@ -324,7 +324,6 @@ public class DistanceFilterLocationProvider extends AbstractLocationProvider imp
     }
 
     private Integer calculateDistanceFilter(Float speed) {
-		speed = (float) 10;
         Double newDistanceFilter = (double) config.getDistanceFilter();
         if (speed < 100) {
             float roundedDistanceFilter = (round(speed / 5) * 5);
